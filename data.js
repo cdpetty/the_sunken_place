f494c7e4a6d642539b7eed9bc5a7a6de24f23fee
Clayton Petty <claytondpetty@gmail.com>
2026-01-26 18:57:16 -0800

    Add: The Sponges - Space Funk '75

--- data.js
+++ data.js
@@ -2,6 +2,7 @@
 // Add new entries at the top of this array
 
 const musicFeed = [
+  { albumArt: "https://i.scdn.co/image/ab67616d00001e024310cecb7b631d80d4129ebd", artist: "The Sponges", title: "Space Funk '75", type: "song", year: 2019, spotifyUrl: "https://open.spotify.com/track/6sGDxKo6gsklfgStjQ8hp0?si=18742720838e4d29" },
   { albumArt: "https://i.scdn.co/image/ab67616d00001e026e0064eb7ad1aed5cb7f7278", artist: "Kyle Watson", title: "Kick Drums", type: "song", year: 2023, spotifyUrl: "https://open.spotify.com/track/0tpw2iisVlCnG0QG8SCIG1?si=b4f0d0b587144edf" },
   { albumArt: "https://i.scdn.co/image/ab67616d00001e029c11e6241d59940a0157c75a", artist: "Gesaffelstein", title: "Opr", type: "song", year: 2011, spotifyUrl: "https://open.spotify.com/track/12Ypr3PCVJ2i7Uwz93q1Vl?si=3a234fb4039746be" },
   { albumArt: "https://i.scdn.co/image/ab67616d00001e02da107bfef518a86314291ed0", artist: "LP Giobbi", title: "Georgia", type: "song", year: 2023, spotifyUrl: "https://open.spotify.com/track/4uY6klDoq8eNCkUvgPkQU1?si=90f8439161b64715" },
